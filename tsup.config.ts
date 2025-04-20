5ebfd34dad742acf27696c7f6c3a0fef323e1a97
Milad Nekofar <milad@nekofar.com>
2025-04-20 05:02:44 +0300

    refactor(tsup): remove `external` and `noExternal` config
    cleaned up unused `external` and `noExternal` options to simplify the configuration and improve maintainability.

--- tsup.config.ts
+++ tsup.config.ts
@@ -20,6 +20,4 @@ export default defineConfig({
   splitting: false,
   clean: true,
   treeshake: true,
-  external: ['viem', 'wagmi', '@wagmi/core', 'react', '@tanstack/react-query'],
-  noExternal: [],
 })
